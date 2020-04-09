5b407093d30cead91e5c68371cf2ca30d74709e0
Steven Kaufman <steveplaysguiola@gmail.com>
2020-04-09 17:17:40 -0500

    removed some boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App renders', () => {
+  const { container } = render(<App />);
+  const app = container.firstChild
+  expect(app).toBeInTheDocument();
 });
