42ded7069fea6b9df43ba83f3197f7324177fc8f
Steven Kaufman <steveplaysguiola@gmail.com>
2020-05-01 22:29:15 -0500

    cleaned up urls for production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
 import feathers from '@feathersjs/client'
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3030/')
+const url = (process.env.PUBLIC_URL === '/react-blog/')
+  ? 'https://steve-kaufman.com:9000'
+  : 'http://localhost:3030'
+
+const socket = io(url)
 const api = feathers()
 
 api.configure(feathers.socketio(socket))
